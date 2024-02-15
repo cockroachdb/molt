1919705d31e331a601fdf59f146fde944c189e90
ZhouXing19 <zhouxing@uchicago.edu>
2024-02-15 12:53:07 -0600

    fetch: add support for enum for schema creation
    If a column is of a user defined enum type, on the target db side, the generated create table statement will include a `create type if not exists` statement before the `create table` statement, so that an equivalenet enum can be created and applied on the newly created schema.
    
    Release note: add support for enum for schema creation

--- fetch/schema_creation_test.go
+++ fetch/schema_creation_test.go
@@ -36,18 +36,18 @@ func TestGetColumnTypes(t *testing.T) {
 			dialect: testutils.PostgresDialect,
 			desc:    "single pk",
 			createTableStatements: []string{`
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    unique_id UUID NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMPTZ,
-    updated_at DATE,
-    is_hired BOOLEAN,
-    age SMALLINT CHECK (age > 18),
-    salary NUMERIC(8, 2),
-    bonus REAL unique
-);
-`},
+		CREATE TABLE employees (
+		   id INT PRIMARY KEY,
+		   unique_id UUID NOT NULL,
+		   name VARCHAR(50) NOT NULL,
+		   created_at TIMESTAMPTZ,
+		   updated_at DATE,
+		   is_hired BOOLEAN,
+		   age SMALLINT CHECK (age > 18),
+		   salary NUMERIC(8, 2),
+		   bonus REAL unique
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
 				"public.employees": {
@@ -98,19 +98,19 @@ CREATE TABLE employees (
 			dialect: testutils.PostgresDialect,
 			desc:    "multiple pks",
 			createTableStatements: []string{`
-CREATE TABLE employees (
-    id INT NOT NULL,
-    unique_id UUID NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMPTZ,
-    updated_at DATE,
-    is_hired BOOLEAN,
-    age SMALLINT CHECK (age > 18),
-    salary NUMERIC(8, 2),
-    bonus REAL unique,
-    CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
-);
-`},
+		CREATE TABLE employees (
+		   id INT NOT NULL,
+		   unique_id UUID NOT NULL,
+		   name VARCHAR(50) NOT NULL,
+		   created_at TIMESTAMPTZ,
+		   updated_at DATE,
+		   is_hired BOOLEAN,
+		   age SMALLINT CHECK (age > 18),
+		   salary NUMERIC(8, 2),
+		   bonus REAL unique,
+		   CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
 				"public.employees": {
@@ -156,6 +156,39 @@ CREATE TABLE employees (
 					"age": {
 						dataType: "smallint",
 						typeOid:  oid.T_int2,
+					},
+				},
+			},
+		},
+		{
+			dialect: testutils.PostgresDialect,
+			desc:    "enums",
+			createTableStatements: []string{`
+CREATE TYPE my_enum_type AS ENUM ('value1', 'value2', 'value3');
+`, `
+CREATE TABLE enum_table (
+    id INT NOT NULL PRIMARY KEY,
+    enum_column my_enum_type,
+    other_column1 TEXT
+);
+`},
+			tableFilter: utils.FilterConfig{TableFilter: `enum_table`},
+			expectedColumnTypes: map[string]map[string]columnWithType{
+				"public.enum_table": {
+					"id": {
+						dataType:     "integer",
+						typeOid:      oid.T_int4,
+						notNullable:  true,
+						isPrimaryKey: true,
+					},
+					"enum_column": {
+						dataType:      "my_enum_type",
+						udtName:       "my_enum_type",
+						udtDefinition: "CREATE TYPE IF NOT EXISTS my_enum_type AS ENUM ('value1', 'value2', 'value3');",
+					},
+					"other_column1": {
+						dataType: "text",
+						typeOid:  oid.T_text,
 					},
 				},
 			},
@@ -208,7 +241,7 @@ CREATE TABLE employees (
 
 			for mt, actualCols := range res {
 				expectedCols := tc.expectedColumnTypes[mt]
-				require.Equal(t, len(actualCols), len(expectedCols))
+				require.Equal(t, len(expectedCols), len(actualCols))
 				for _, actualCol := range actualCols {
 					require.NoError(t, checkIfColInfoEqual(actualCol, expectedCols[actualCol.columnName]))
 				}
@@ -235,15 +268,25 @@ func checkIfColInfoEqual(actual, expected columnWithType) error {
 	if actual.dataType != expected.dataType {
 		return errors.AssertionFailedf("[%s] expected datatype: %s, but got: %s", actual.Name(), expected.dataType, actual.dataType)
 	}
-	if actual.typeOid != expected.typeOid {
-		return errors.AssertionFailedf("[%s] expected typeOid: %s, but got: %s", actual.Name(), expected.typeOid, actual.typeOid)
-	}
 	if actual.notNullable != expected.notNullable {
 		return errors.AssertionFailedf("[%s] expected notNullable: %t, but got: %t", actual.Name(), expected.notNullable, actual.notNullable)
 	}
 	if actual.isPrimaryKey != expected.isPrimaryKey {
 		return errors.AssertionFailedf("[%s] expected isPrimaryKey: %t, but got: %t", actual.Name(), expected.isPrimaryKey, actual.isPrimaryKey)
 	}
+	if expected.udtName != "" {
+		if actual.udtName != expected.udtName {
+			return errors.AssertionFailedf("[%s] expected udtName: %s, but got: %s", actual.Name(), expected.udtName, actual.udtName)
+		}
+		if actual.udtDefinition != expected.udtDefinition {
+			return errors.AssertionFailedf("[%s] expected udtDefinition: %s, but got: %s", actual.Name(), expected.udtDefinition, actual.udtDefinition)
+		}
+	} else {
+		if actual.typeOid != expected.typeOid {
+			return errors.AssertionFailedf("[%s] expected typeOid: %s, but got: %s", actual.Name(), expected.typeOid, actual.typeOid)
+		}
+	}
+
 	return nil
 }
 
@@ -265,18 +308,18 @@ func TestCreateTableStatement(t *testing.T) {
 			dialect: testutils.PostgresDialect,
 			desc:    "single pk",
 			createTableStatements: []string{`
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    unique_id UUID NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMPTZ,
-    updated_at DATE,
-    is_hired BOOLEAN,
-    age SMALLINT CHECK (age > 18),
-    salary NUMERIC(8, 2),
-    bonus REAL unique
-);
-`},
+		CREATE TABLE employees (
+		   id INT PRIMARY KEY,
+		   unique_id UUID NOT NULL,
+		   name VARCHAR(50) NOT NULL,
+		   created_at TIMESTAMPTZ,
+		   updated_at DATE,
+		   is_hired BOOLEAN,
+		   age SMALLINT CHECK (age > 18),
+		   salary NUMERIC(8, 2),
+		   bonus REAL unique
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedCreateTableStmts: []string{
 				`CREATE TABLE employees (id INT4 NOT NULL PRIMARY KEY, unique_id UUID NOT NULL, name VARCHAR NOT NULL, created_at TIMESTAMPTZ, updated_at DATE, is_hired BOOL, age INT2, salary DECIMAL, bonus FLOAT4)`,
@@ -286,19 +329,19 @@ CREATE TABLE employees (
 			dialect: testutils.PostgresDialect,
 			desc:    "multiple pks",
 			createTableStatements: []string{`
-CREATE TABLE employees (
-    id INT NOT NULL,
-    unique_id UUID NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    created_at TIMESTAMPTZ,
-    updated_at DATE,
-    is_hired BOOLEAN,
-    age SMALLINT CHECK (age > 18),
-    salary NUMERIC(8, 2),
-    bonus REAL unique,
-    CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
-);
-`},
+		CREATE TABLE employees (
+		   id INT NOT NULL,
+		   unique_id UUID NOT NULL,
+		   name VARCHAR(50) NOT NULL,
+		   created_at TIMESTAMPTZ,
+		   updated_at DATE,
+		   is_hired BOOLEAN,
+		   age SMALLINT CHECK (age > 18),
+		   salary NUMERIC(8, 2),
+		   bonus REAL unique,
+		   CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedCreateTableStmts: []string{
 				`CREATE TABLE employees (id INT4 NOT NULL, unique_id UUID NOT NULL, name VARCHAR NOT NULL, created_at TIMESTAMPTZ NOT NULL, updated_at DATE, is_hired BOOL, age INT2, salary DECIMAL, bonus FLOAT4, CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at))`,
@@ -308,19 +351,19 @@ CREATE TABLE employees (
 			dialect: testutils.PostgresDialect,
 			desc:    "foreign key is ignored",
 			createTableStatements: []string{`
-CREATE TABLE department (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(50) NOT NULL
-);
-
-`,
+		CREATE TABLE department (
+		   department_id SERIAL PRIMARY KEY,
+		   department_name VARCHAR(50) NOT NULL
+		);
+		
+		`,
 				`
-CREATE TABLE employee (
-    employee_id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(50) NOT NULL,
-    department_id INT REFERENCES department(department_id) ON DELETE CASCADE
-);
-`},
+		CREATE TABLE employee (
+		   employee_id SERIAL PRIMARY KEY,
+		   employee_name VARCHAR(50) NOT NULL,
+		   department_id INT REFERENCES department(department_id) ON DELETE CASCADE
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employee`},
 			expectedCreateTableStmts: []string{
 				`CREATE TABLE employee (employee_id INT4 NOT NULL PRIMARY KEY, employee_name VARCHAR NOT NULL, department_id INT4)`,
@@ -330,25 +373,42 @@ CREATE TABLE employee (
 			dialect: testutils.PostgresDialect,
 			desc:    "unique, check and 2nd index are ignored",
 			createTableStatements: []string{`
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    age INTEGER,
-    address VARCHAR(50) NOT NULL,
-    start_date DATE,
-    end_date DATE,
-    CONSTRAINT check_dates CHECK (start_date <= end_date),  -- Check Constraint
-    CONSTRAINT unique_constraint_name UNIQUE (start_date)  -- Secondary index
-);
-
-`,
+		CREATE TABLE employee (
+		   id SERIAL PRIMARY KEY,
+		   name VARCHAR(50) UNIQUE NOT NULL,
+		   age INTEGER,
+		   address VARCHAR(50) NOT NULL,
+		   start_date DATE,
+		   end_date DATE,
+		   CONSTRAINT check_dates CHECK (start_date <= end_date),  -- Check Constraint
+		   CONSTRAINT unique_constraint_name UNIQUE (start_date)  -- Secondary index
+		);
+		
+		`,
 				`
-CREATE UNIQUE INDEX my_unique_idx ON employee(age);
-`},
+		CREATE UNIQUE INDEX my_unique_idx ON employee(age);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `employee`},
 			expectedCreateTableStmts: []string{
 				`CREATE TABLE employee (id INT4 NOT NULL PRIMARY KEY, name VARCHAR NOT NULL, age INT4, address VARCHAR NOT NULL, start_date DATE, end_date DATE)`,
 			},
+		},
+		{
+			dialect: testutils.PostgresDialect,
+			desc:    "enum column",
+			createTableStatements: []string{`
+CREATE TYPE my_enum_type AS ENUM ('value1', 'value2', 'value3');
+`, `
+CREATE TABLE enum_table (
+    id INT NOT NULL PRIMARY KEY,
+    enum_column my_enum_type,
+    other_column1 TEXT
+);
+`,
+			},
+			tableFilter: utils.FilterConfig{TableFilter: `enum_table`},
+			expectedCreateTableStmts: []string{
+				` CREATE TYPE IF NOT EXISTS my_enum_type AS ENUM ('value1', 'value2', 'value3'); CREATE TABLE enum_table (id INT4 NOT NULL PRIMARY KEY, enum_column my_enum_type, other_column1 STRING)`},
 		},
 	} {
 		t.Run(fmt.Sprintf("%s/%s", tc.dialect.String(), tc.desc), func(t *testing.T) {
