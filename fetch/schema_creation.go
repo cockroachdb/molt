9b85ee5d81bf90da35edaf92af848dd0b509f45e
ZhouXing19 <zhouxing@uchicago.edu>
2024-02-15 12:53:07 -0600

    fetch: add support for enum for schema creation
    If a column is of a user defined enum type, on the target db side, the generated create table statement will include a `create type if not exists` statement before the `create table` statement, so that an equivalenet enum can be created and applied on the newly created schema.
    
    Release note: add support for enum for schema creation

--- fetch/schema_creation.go
+++ fetch/schema_creation.go
@@ -3,6 +3,7 @@ package fetch
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/cockroachdb/cockroachdb-parser/pkg/sql/parser"
 	"github.com/cockroachdb/cockroachdb-parser/pkg/sql/sem/tree"
@@ -39,7 +40,11 @@ func (cs columnsWithType) CRDBCreateTableStmt() (string, error) {
 	// that group all the selected columns, thus result in different syntax.
 	includePkForEachCol := len(pkList) <= 1
 	for _, col := range cs {
-		res.Defs = append(res.Defs, col.CRDBColDef(includePkForEachCol))
+		colDef, err := col.CRDBColDef(includePkForEachCol)
+		if err != nil {
+			return "", err
+		}
+		res.Defs = append(res.Defs, colDef)
 	}
 
 	if !includePkForEachCol {
@@ -61,19 +66,36 @@ func (cs columnsWithType) CRDBCreateTableStmt() (string, error) {
 }
 
 type columnWithType struct {
-	schemaName   string
-	tableName    string
-	columnName   string
-	dataType     string
-	typeOid      oid.Oid
-	notNullable  bool
-	isPrimaryKey bool
+	schemaName    string
+	tableName     string
+	columnName    string
+	dataType      string
+	typeOid       oid.Oid
+	notNullable   bool
+	isPrimaryKey  bool
+	udtName       string
+	udtDefinition string
 }
 
-func (t *columnWithType) CRDBColDef(includePk bool) *tree.ColumnTableDef {
+func (t *columnWithType) CRDBColDef(includePk bool) (*tree.ColumnTableDef, error) {
+	var colType tree.ResolvableTypeReference
+	var err error
+	if t.udtDefinition != "" {
+		if t.udtName == "" {
+			// This should not happen, but as a sanity check.
+			return nil, errors.AssertionFailedf("user defined type definition %q is not null, but the type name is null", t.udtDefinition)
+		}
+		colType, err = tree.NewUnresolvedObjectName(1 /* numParts */, [3]string{t.udtName, "", ""}, tree.NoAnnotation /* annotation idx */)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unable to parse the type name %q", t.udtName)
+		}
+	} else {
+		colType = crdbtypes.OidToType[t.typeOid]
+	}
+
 	res := &tree.ColumnTableDef{
 		Name: tree.Name(t.columnName),
-		Type: crdbtypes.OidToType[t.typeOid],
+		Type: colType,
 	}
 	if !t.notNullable {
 		res.Nullable.Nullability = parser.NULL
@@ -81,7 +103,7 @@ func (t *columnWithType) CRDBColDef(includePk bool) *tree.ColumnTableDef {
 	if t.isPrimaryKey && includePk {
 		res.PrimaryKey.IsPrimaryKey = true
 	}
-	return res
+	return res, nil
 }
 
 func (t *columnWithType) Name() string {
@@ -98,38 +120,76 @@ func GetColumnTypes(
 ) (columnsWithType, error) {
 	const (
 		pgQuery = `SELECT
-    c.relnamespace::regnamespace::text as schema_name,
+    t1.schema_name,
+    t1.table_name,
+    t1.column_name,
+    t1.data_type,
+    t1.type_oid,
+    t1.not_nullable,
+    t1.is_primary_key,
+    COALESCE(t2.udt_name, '') AS enum_type,
+    COALESCE(t2.udt_def, '') AS enum_type_definition
+FROM (
+    SELECT
+        c.relnamespace::regnamespace::text AS schema_name,
         c.relname AS table_name,
-    a.attname AS column_name,
-    format_type(a.atttypid, a.atttypmod) AS data_type,
-    a.atttypid AS type_oid,
-    a.attnotnull AS not_nullable,
-    CASE
-        WHEN a.attname IN (
-            SELECT column_name
-            FROM information_schema.constraint_column_usage
-            WHERE constraint_name = (
-                SELECT constraint_name
-                FROM information_schema.table_constraints
-                WHERE table_name = c.relname
-                  AND constraint_type = 'PRIMARY KEY'
-            )
-        ) THEN true
-        ELSE false
+        a.attname AS column_name,
+        format_type(a.atttypid, a.atttypmod) AS data_type,
+        a.atttypid AS type_oid,
+        a.attnotnull AS not_nullable,
+        CASE
+            WHEN a.attname IN (
+                SELECT column_name
+                FROM information_schema.constraint_column_usage
+                WHERE constraint_name = (
+                    SELECT constraint_name
+                    FROM information_schema.table_constraints
+                    WHERE table_name = c.relname
+                      AND constraint_type = 'PRIMARY KEY'
+                )
+            ) THEN true
+            ELSE false
         END AS is_primary_key
-FROM
-    pg_catalog.pg_class c
-        JOIN
-    pg_catalog.pg_attribute a ON c.oid = a.attrelid
-        LEFT JOIN
-    pg_catalog.pg_index ix ON c.oid = ix.indrelid AND a.attnum = ANY(ix.indkey)
-WHERE
+    FROM
+        pg_catalog.pg_class c
+    JOIN pg_catalog.pg_attribute a ON c.oid = a.attrelid
+    LEFT JOIN pg_catalog.pg_index ix ON c.oid = ix.indrelid AND a.attnum = ANY(ix.indkey)
+    WHERE
         c.relkind = 'r'  -- 'r' indicates a table (relation)
-  AND a.attnum > 0 -- Exclude system columns
-  AND c.relname = $1
-  AND c.relnamespace::regnamespace::text = $2
+        AND a.attnum > 0 -- Exclude system columns
+        AND c.relname = $1
+        AND c.relnamespace::regnamespace::text = $2
+) t1
+LEFT JOIN (
+    SELECT
+        c.column_name,
+        c.table_name,
+        c.table_schema,
+        c.udt_name,
+        t.definition AS udt_def,
+        c.ordinal_position
+    FROM
+        information_schema.columns c
+    LEFT JOIN (
+        SELECT
+            'CREATE TYPE IF NOT EXISTS ' || t.typname || ' AS ENUM ' ||
+            '(' || string_agg(quote_literal(e.enumlabel), ', ' ORDER BY e.enumsortorder) || ');' AS definition,
+            t.typname
+        FROM
+            pg_type t
+        JOIN pg_enum e ON t.oid = e.enumtypid
+        GROUP BY
+            t.typname
+    ) t ON c.udt_name = t.typname
+    WHERE
+        c.table_name = $1 AND c.table_schema = $2
+) t2 ON t1.column_name = t2.column_name
+    AND t1.table_name = t2.table_name
+    AND t1.schema_name = t2.table_schema
 ORDER BY
-    table_name, a.attnum;`
+    t1.table_name,
+    t2.ordinal_position;
+`
 	)
 
 	res := make([]columnWithType, 0)
@@ -143,7 +203,7 @@ ORDER BY
 		}
 		for rows.Next() {
 			newCol := columnWithType{}
-			if err := rows.Scan(&newCol.schemaName, &newCol.tableName, &newCol.columnName, &newCol.dataType, &newCol.typeOid, &newCol.notNullable, &newCol.isPrimaryKey); err != nil {
+			if err := rows.Scan(&newCol.schemaName, &newCol.tableName, &newCol.columnName, &newCol.dataType, &newCol.typeOid, &newCol.notNullable, &newCol.isPrimaryKey, &newCol.udtName, &newCol.udtDefinition); err != nil {
 				return nil, errors.Wrap(err, "failed to scan query result to a columnWithType object")
 			}
 			logger.Debug().Msgf("collect column:%s", newCol.String())
@@ -178,9 +238,21 @@ func GetCreateTableStmt(
 	if err != nil {
 		return "", errors.Wrapf(err, "failed get columns for target table: %s", table.String())
 	}
+
+	var res string
+	for _, col := range newCols {
+		if col.udtDefinition != "" {
+			logger.Info().Msgf("the original schema contains enum type %q. A tentative enum type will be created as %q", col.udtName, col.udtDefinition)
+			res = strings.Join([]string{res, col.udtDefinition}, " ")
+		}
+	}
 	createTableStmt, err := newCols.CRDBCreateTableStmt()
 	if err != nil {
 		return "", err
+	}
+
+	if res != "" {
+		return strings.Join([]string{res, createTableStmt}, " "), nil
 	}
 	return createTableStmt, nil
 }
