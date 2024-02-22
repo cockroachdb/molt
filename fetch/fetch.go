8bb9a28deb215764b76226ea0372aff9fa1f4968
ZhouXing19 <zhouxing@uchicago.edu>
2024-02-22 10:32:57 -0500

    get type tests

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -152,7 +152,7 @@ func Fetch(
 				}
 				logger.Debug().Msgf("finished dropping table with %q", dropTableStmt)
 
-				createTableStmt, err := GetCreateTableStmt(ctx, logger, conns[0], t)
+				createTableStmt, err := GetCreateTableStmt(ctx, logger, conns[0], t, false /* testOnly */)
 				if err != nil {
 					return err
 				}
--- fetch/schema_creation_test.go
+++ fetch/schema_creation_test.go
@@ -30,24 +30,23 @@ func TestGetColumnTypes(t *testing.T) {
 
 	const dbName = "get_column_types"
 
-	// TODO(janexing): add crdb and mysql.
 	for idx, tc := range []testcase{
 		{
 			dialect: testutils.PostgresDialect,
 			desc:    "single pk",
 			createTableStatements: []string{`
-		CREATE TABLE employees (
-		   id INT PRIMARY KEY,
-		   unique_id UUID NOT NULL,
-		   name VARCHAR(50) NOT NULL,
-		   created_at TIMESTAMPTZ,
-		   updated_at DATE,
-		   is_hired BOOLEAN,
-		   age SMALLINT CHECK (age > 18),
-		   salary NUMERIC(8, 2),
-		   bonus REAL unique
-		);
-		`},
+				CREATE TABLE employees (
+				   id INT PRIMARY KEY,
+				   unique_id UUID NOT NULL,
+				   name VARCHAR(50) NOT NULL,
+				   created_at TIMESTAMPTZ,
+				   updated_at DATE,
+				   is_hired BOOLEAN,
+				   age SMALLINT CHECK (age > 18),
+				   salary NUMERIC(8, 2),
+				   bonus REAL unique
+				);
+				`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
 				"public.employees": {
@@ -101,19 +100,19 @@ func TestGetColumnTypes(t *testing.T) {
 			dialect: testutils.PostgresDialect,
 			desc:    "multiple pks",
 			createTableStatements: []string{`
-		CREATE TABLE employees (
-		   id INT NOT NULL,
-		   unique_id UUID NOT NULL,
-		   name VARCHAR(50) NOT NULL,
-		   created_at TIMESTAMPTZ,
-		   updated_at DATE,
-		   is_hired BOOLEAN,
-		   age SMALLINT CHECK (age > 18),
-		   salary NUMERIC(8, 2),
-		   bonus REAL unique,
-		   CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
-		);
-		`},
+				CREATE TABLE employees (
+				   id INT NOT NULL,
+				   unique_id UUID NOT NULL,
+				   name VARCHAR(50) NOT NULL,
+				   created_at TIMESTAMPTZ,
+				   updated_at DATE,
+				   is_hired BOOLEAN,
+				   age SMALLINT CHECK (age > 18),
+				   salary NUMERIC(8, 2),
+				   bonus REAL unique,
+				   CONSTRAINT "primary" PRIMARY KEY (id, unique_id, created_at)
+				);
+				`},
 			tableFilter: utils.FilterConfig{TableFilter: `employees`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
 				"public.employees": {
@@ -168,14 +167,14 @@ func TestGetColumnTypes(t *testing.T) {
 			dialect: testutils.PostgresDialect,
 			desc:    "enums",
 			createTableStatements: []string{`
-CREATE TYPE my_enum_type AS ENUM ('value1', 'value2', 'value3');
-`, `
-CREATE TABLE enum_table (
-    id INT NOT NULL PRIMARY KEY,
-    enum_column my_enum_type,
-    other_column1 TEXT
-);
-`},
+		CREATE TYPE my_enum_type AS ENUM ('value1', 'value2', 'value3');
+		`, `
+		CREATE TABLE enum_table (
+		   id INT NOT NULL PRIMARY KEY,
+		   enum_column my_enum_type,
+		   other_column1 TEXT
+		);
+		`},
 			tableFilter: utils.FilterConfig{TableFilter: `enum_table`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
 				"public.enum_table": {
@@ -202,60 +201,287 @@ CREATE TABLE enum_table (
 			dialect: testutils.MySQLDialect,
 			desc:    "single pk",
 			createTableStatements: []string{`
-		CREATE TABLE employees (
-		   id INT PRIMARY KEY,
-		   unique_id UUID NOT NULL,
-		   name VARCHAR(50) NOT NULL,
-		   created_at TIMESTAMPTZ,
-		   updated_at DATE,
-		   is_hired BOOLEAN,
-		   age SMALLINT CHECK (age > 18),
-		   salary NUMERIC(8, 2),
-		   bonus REAL unique
-		);
+CREATE TABLE test_table (
+    integer_col INT PRIMARY KEY,
+    smallint_col SMALLINT,
+    bigint_col BIGINT NOT NULL,
+    decimal_col DECIMAL(10,2),
+    float_col FLOAT,
+    double_col DOUBLE,
+    bit_col BIT NOT NULL,
+    date_col DATE,
+    datetime_col DATETIME,
+    timestamp_col TIMESTAMP,
+    time_col TIME NOT NULL,
+    char_col CHAR(10),
+    varchar_col VARCHAR(255),
+    binary_col BINARY(10) UNIQUE,
+    varbinary_col VARBINARY(255),
+    blob_col BLOB,
+    text_col TEXT NOT NULL,
+    mediumtext_col MEDIUMTEXT,
+    longtext_col LONGTEXT,
+    json_col JSON,
+    enum_col ENUM('value1', 'value2', 'value3'),
+    set_col SET('value1', 'value2', 'value3')
+);
 		`},
-			tableFilter: utils.FilterConfig{TableFilter: `employees`},
+			tableFilter: utils.FilterConfig{TableFilter: `test_table`},
 			expectedColumnTypes: map[string]map[string]columnWithType{
-				"public.employees": {
-					"id": {
-						dataType:     "integer",
-						typeOid:      oid.T_int4,
-						notNullable:  true,
+				"get_column_types.test_table": {
+					"integer_col": {
+						dataType:     "int",
 						isPrimaryKey: true,
+						typeOid:      oid.T_int4,
 					},
-					"name": {
-						dataType:    "character varying(50)",
-						typeOid:     oid.T_varchar,
-						notNullable: true,
+					"smallint_col": {
+						dataType: "smallint",
+						nullable: true,
+						typeOid:  oid.T_int2,
 					},
-					"created_at": {
-						dataType: "timestamp with time zone",
-						typeOid:  oid.T_timestamptz,
+					"bigint_col": {
+						dataType: "bigint",
+						typeOid:  oid.T_int8,
 					},
-					"is_hired": {
-						dataType: "boolean",
-						typeOid:  oid.T_bool,
-					},
-					"salary": {
-						dataType: "numeric(8,2)",
+					"decimal_col": {
+						dataType: "decimal",
+						nullable: true,
 						typeOid:  oid.T_numeric,
 					},
-					"bonus": {
-						dataType: "real",
+					"float_col": {
+						dataType: "float",
+						nullable: true,
 						typeOid:  oid.T_float4,
 					},
-					"unique_id": {
-						dataType:    "uuid",
-						typeOid:     oid.T_uuid,
-						notNullable: true,
+					"double_col": {
+						dataType: "double",
+						nullable: true,
+						typeOid:  oid.T_float8,
 					},
-					"updated_at": {
+					"bit_col": {
+						dataType: "bit",
+						typeOid:  oid.T_varbit,
+					},
+					"date_col": {
 						dataType: "date",
+						nullable: true,
 						typeOid:  oid.T_date,
 					},
-					"age": {
-						dataType: "smallint",
-						typeOid:  oid.T_int2,
+					"datetime_col": {
+						dataType: "datetime",
+						nullable: true,
+						typeOid:  oid.T_timestamp,
+					},
+					"timestamp_col": {
+						dataType: "timestamp",
+						nullable: true,
+						typeOid:  oid.T_timestamptz,
+					},
+					"time_col": {
+						dataType: "time",
+						typeOid:  oid.T_time,
+					},
+					"char_col": {
+						dataType: "char",
+						nullable: true,
+						typeOid:  oid.T_varchar,
+					},
+					"varchar_col": {
+						dataType: "varchar",
+						nullable: true,
+						typeOid:  oid.T_varchar,
+					},
+					"binary_col": {
+						dataType: "binary",
+						nullable: true,
+						typeOid:  oid.T_bytea,
+					},
+					"varbinary_col": {
+						dataType: "varbinary",
+						nullable: true,
+						typeOid:  oid.T_bytea,
+					},
+					"blob_col": {
+						dataType: "blob",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"text_col": {
+						dataType: "text",
+						typeOid:  oid.T_text,
+					},
+					"mediumtext_col": {
+						dataType: "mediumtext",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"longtext_col": {
+						dataType: "longtext",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"json_col": {
+						dataType: "json",
+						nullable: true,
+						typeOid:  oid.T_jsonb,
+					},
+					"enum_col": {
+						dataType:      "enum",
+						nullable:      true,
+						typeOid:       oid.T_anyenum,
+						udtName:       `get_column_types_test_table_enum_col_enum`,
+						udtDefinition: `CREATE TYPE IF NOT EXISTS get_column_types_test_table_enum_col_enum AS ENUM ('value1','value2','value3')`,
+					},
+					"set_col": {
+						dataType: "set",
+						nullable: true,
+					},
+				},
+			},
+		},
+		{
+			dialect: testutils.MySQLDialect,
+			desc:    "multiple pk",
+			createTableStatements: []string{`
+CREATE TABLE test_table_multi_pk (
+    integer_col INT,
+    smallint_col SMALLINT,
+    bigint_col BIGINT,
+    decimal_col DECIMAL(10,2),
+    float_col FLOAT,
+    double_col DOUBLE,
+    bit_col BIT,
+    date_col DATE,
+    datetime_col DATETIME,
+    timestamp_col TIMESTAMP,
+    time_col TIME,
+    char_col CHAR(10),
+    varchar_col VARCHAR(255),
+    binary_col BINARY(10),
+    varbinary_col VARBINARY(255),
+    blob_col BLOB,
+    text_col TEXT,
+    mediumtext_col MEDIUMTEXT,
+    longtext_col LONGTEXT,
+    json_col JSON,
+    enum_col ENUM('value1', 'value2', 'value3'),
+    set_col SET('value1', 'value2', 'value3'),
+    PRIMARY KEY (integer_col, smallint_col)
+);
+		`},
+			tableFilter: utils.FilterConfig{TableFilter: `test_table_multi_pk`},
+			expectedColumnTypes: map[string]map[string]columnWithType{
+				"get_column_types.test_table_multi_pk": {
+					"integer_col": {
+						dataType:     "int",
+						isPrimaryKey: true,
+						typeOid:      oid.T_int4,
+					},
+					"smallint_col": {
+						dataType:     "smallint",
+						isPrimaryKey: true,
+						typeOid:      oid.T_int2,
+					},
+					"bigint_col": {
+						dataType: "bigint",
+						nullable: true,
+						typeOid:  oid.T_int8,
+					},
+					"decimal_col": {
+						dataType: "decimal",
+						nullable: true,
+						typeOid:  oid.T_numeric,
+					},
+					"float_col": {
+						dataType: "float",
+						nullable: true,
+						typeOid:  oid.T_float4,
+					},
+					"double_col": {
+						dataType: "double",
+						nullable: true,
+						typeOid:  oid.T_float8,
+					},
+					"bit_col": {
+						dataType: "bit",
+						nullable: true,
+						typeOid:  oid.T_varbit,
+					},
+					"date_col": {
+						dataType: "date",
+						nullable: true,
+						typeOid:  oid.T_date,
+					},
+					"datetime_col": {
+						dataType: "datetime",
+						nullable: true,
+						typeOid:  oid.T_timestamp,
+					},
+					"timestamp_col": {
+						dataType: "timestamp",
+						nullable: true,
+						typeOid:  oid.T_timestamptz,
+					},
+					"time_col": {
+						dataType: "time",
+						nullable: true,
+						typeOid:  oid.T_time,
+					},
+					"char_col": {
+						dataType: "char",
+						nullable: true,
+						typeOid:  oid.T_varchar,
+					},
+					"varchar_col": {
+						dataType: "varchar",
+						nullable: true,
+						typeOid:  oid.T_varchar,
+					},
+					"binary_col": {
+						dataType: "binary",
+						nullable: true,
+						typeOid:  oid.T_bytea,
+					},
+					"varbinary_col": {
+						dataType: "varbinary",
+						nullable: true,
+						typeOid:  oid.T_bytea,
+					},
+					"blob_col": {
+						dataType: "blob",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"text_col": {
+						dataType: "text",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"mediumtext_col": {
+						dataType: "mediumtext",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"longtext_col": {
+						dataType: "longtext",
+						nullable: true,
+						typeOid:  oid.T_text,
+					},
+					"json_col": {
+						dataType: "json",
+						nullable: true,
+						typeOid:  oid.T_jsonb,
+					},
+					"enum_col": {
+						dataType:      "enum",
+						nullable:      true,
+						typeOid:       oid.T_anyenum,
+						udtName:       `get_column_types_test_table_multi_pk_enum_col_enum`,
+						udtDefinition: `CREATE TYPE IF NOT EXISTS get_column_types_test_table_multi_pk_enum_col_enum AS ENUM ('value1','value2','value3')`,
+					},
+					"set_col": {
+						dataType: "set",
+						nullable: true,
 					},
 				},
 			},
@@ -268,6 +494,9 @@ CREATE TABLE enum_table (
 			switch tc.dialect {
 			case testutils.PostgresDialect:
 				conns[0], err = dbconn.TestOnlyCleanDatabase(ctx, "source", testutils.PGConnStr(), fmt.Sprintf("%s-%d", dbName, idx))
+				require.NoError(t, err)
+			case testutils.MySQLDialect:
+				conns[0], err = dbconn.TestOnlyCleanDatabase(ctx, "source", testutils.MySQLConnStr(), dbName)
 				require.NoError(t, err)
 			default:
 				t.Fatalf("unsupported dialect: %s", tc.dialect.String())
@@ -298,7 +527,7 @@ CREATE TABLE enum_table (
 			res := make(map[string]map[string]columnWithType)
 
 			for _, missingTable := range missingTables {
-				newCols, err := GetColumnTypes(ctx, logger, conns[0], missingTable.DBTable)
+				newCols, err := GetColumnTypes(ctx, logger, conns[0], missingTable.DBTable, true /* testOnly */)
 				require.NoError(t, err)
 				res[missingTable.String()] = make(map[string]columnWithType)
 				for _, c := range newCols {
@@ -306,13 +535,18 @@ CREATE TABLE enum_table (
 				}
 			}
 
+			var err1 error
 			for mt, actualCols := range res {
 				expectedCols := tc.expectedColumnTypes[mt]
 				require.Equal(t, len(expectedCols), len(actualCols))
 				for _, actualCol := range actualCols {
-					require.NoError(t, checkIfColInfoEqual(actualCol, expectedCols[actualCol.columnName]))
+					if err = checkIfColInfoEqual(actualCol, expectedCols[actualCol.columnName]); err != nil {
+						err1 = err
+						t.Log(err)
+					}
 				}
 			}
+			require.NoError(t, err1)
 			t.Logf("test passed!")
 		})
 	}
@@ -341,6 +575,9 @@ func checkIfColInfoEqual(actual, expected columnWithType) error {
 	if actual.isPrimaryKey != expected.isPrimaryKey {
 		return errors.AssertionFailedf("[%s] expected isPrimaryKey: %t, but got: %t", actual.Name(), expected.isPrimaryKey, actual.isPrimaryKey)
 	}
+	if expected.typeOid != 0 && actual.typeOid != expected.typeOid {
+		return errors.AssertionFailedf("[%s] expected typeOid: %s, but got: %s", actual.Name(), expected.typeOid, actual.typeOid)
+	}
 	if expected.udtName != "" {
 		if actual.udtName != expected.udtName {
 			return errors.AssertionFailedf("[%s] expected udtName: %s, but got: %s", actual.Name(), expected.udtName, actual.udtName)
@@ -348,12 +585,7 @@ func checkIfColInfoEqual(actual, expected columnWithType) error {
 		if actual.udtDefinition != expected.udtDefinition {
 			return errors.AssertionFailedf("[%s] expected udtDefinition: %s, but got: %s", actual.Name(), expected.udtDefinition, actual.udtDefinition)
 		}
-	} else {
-		if actual.typeOid != expected.typeOid {
-			return errors.AssertionFailedf("[%s] expected typeOid: %s, but got: %s", actual.Name(), expected.typeOid, actual.typeOid)
-		}
 	}
-
 	return nil
 }
 
@@ -509,7 +741,7 @@ CREATE TABLE enum_table (
 			require.Equal(t, len(tc.expectedCreateTableStmts), len(missingTables))
 
 			for i, missingTable := range missingTables {
-				actualCreateTableStmt, err := GetCreateTableStmt(ctx, logger, conns[0], missingTable.DBTable)
+				actualCreateTableStmt, err := GetCreateTableStmt(ctx, logger, conns[0], missingTable.DBTable, true /* testOnly */)
 				require.NoError(t, err)
 				require.Equal(t, tc.expectedCreateTableStmts[i], actualCreateTableStmt)
 			}
