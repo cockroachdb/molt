a3b9fe13f5d218320d11dd2961bb61b607bdac42
ZhouXing19 <zhouxing@uchicago.edu>
2024-03-27 13:50:24 -0500

    successfully parse json

--- utils/typeconv/map_override.go
+++ utils/typeconv/map_override.go
@@ -1,7 +1,48 @@
 package typeconv
 
-type OverrideTypeMap struct {
-	ColumnTypeMaps []ColumnTypeMap `json:"column-type-maps"`
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/cockroachdb/errors"
+)
+
+// OverrideTypeMap correspond to json of the following format:
+// [
+//
+//	{
+//	  "column": "age",
+//	  "type-kvs": [
+//	    {
+//	      "source-type": "int",
+//	      "crdb-type": "INT"
+//	    },
+//	    {
+//	      "source-type": "float",
+//	      "crdb-type": "FLOAT"
+//	    }
+//	  ]
+//	},
+//	{
+//	  "column": "name",
+//	  "type-kvs": [
+//	    {
+//	      "source-type": "string",
+//	      "crdb-type": "TEXT"
+//	    }
+//	  ]
+//	}
+//
+// ]
+type OverrideTypeMap []ColumnTypeMap
+
+func (m *OverrideTypeMap) String() string {
+	jsonData, err := json.Marshal(m)
+	// This should not happen but just in case.
+	if err != nil {
+		panic(errors.Wrap(err, "cannot get the json representation of the type override map"))
+	}
+	return string(jsonData)
 }
 
 type ColumnTypeMap struct {
@@ -12,4 +53,16 @@ type ColumnTypeMap struct {
 type TypeKV struct {
 	SourceType string `json:"source-type"`
 	CRDBType   string `json:"crdb-type"`
+}
+
+func OverrideTypeMapFromFile(filepath string) (*OverrideTypeMap, error) {
+	var res = &OverrideTypeMap{}
+	bytesValus, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read json file %s for type map", filepath)
+	}
+	if err := json.Unmarshal(bytesValus, res); err != nil {
+		return nil, err
+	}
+	return res, nil
 }
